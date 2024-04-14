5582f4820472e5781a0e5652a8a76444b0d4ce45
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-04-13 21:43:11 -0400

    use xcompat for copperblock and remove checks that prevent issues on non mtg before

--- home_vending_machines/crafts.lua
+++ home_vending_machines/crafts.lua
@@ -1,33 +1,26 @@
 local materials = xcompat.materials
 
-if minetest.get_modpath("default") then
-    minetest.register_craft({
-        output = "home_workshop_misc:soda_machine",
-        recipe = {
-            {materials.steel_ingot, materials.steel_ingot, materials.steel_ingot},
-            {materials.steel_ingot, materials.dye_red, materials.steel_ingot},
-            {materials.steel_ingot, "default:copperblock", materials.steel_ingot},
-        },
-    })
-end
-if minetest.get_modpath("vessels") then
-    minetest.register_craft({
-        output = "home_vending_machines:drink_machine",
-        recipe = {
-            {materials.steel_ingot, "group:vessel", materials.steel_ingot},
-            {materials.steel_ingot, materials.steel_ingot, materials.steel_ingot},
-            {materials.steel_ingot, "default:copperblock", materials.steel_ingot},
-        },
-    })
-end
-if minetest.global_exists("farming") and farming.mod == "redo" then
-    minetest.register_craft({
-        output = "home_vending_machines:sweet_machine",
-        recipe = {
-            {materials.steel_ingot, "group:food_sugar",    materials.steel_ingot},
-            {materials.steel_ingot, materials.steel_ingot, materials.steel_ingot},
-            {materials.steel_ingot, "default:copperblock", materials.steel_ingot},
-        },
-    })
-end
-
+minetest.register_craft({
+    output = "home_workshop_misc:soda_machine",
+    recipe = {
+        {materials.steel_ingot, materials.steel_ingot, materials.steel_ingot},
+        {materials.steel_ingot, materials.dye_red, materials.steel_ingot},
+        {materials.steel_ingot, materials.copper_block, materials.steel_ingot},
+    },
+})
+minetest.register_craft({
+    output = "home_vending_machines:drink_machine",
+    recipe = {
+        {materials.steel_ingot, "group:vessel", materials.steel_ingot},
+        {materials.steel_ingot, materials.steel_ingot, materials.steel_ingot},
+        {materials.steel_ingot, materials.copper_block, materials.steel_ingot},
+    },
+})
+minetest.register_craft({
+    output = "home_vending_machines:sweet_machine",
+    recipe = {
+        {materials.steel_ingot, "group:food_sugar",    materials.steel_ingot},
+        {materials.steel_ingot, materials.steel_ingot, materials.steel_ingot},
+        {materials.steel_ingot, materials.copper_block, materials.steel_ingot},
+    },
+})
